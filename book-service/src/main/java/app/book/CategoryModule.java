3cb9897b349224f1d33b5090a80addabf45d3699
Meow <liuguozhu720@gmail.com>
2020-06-22 23:19:18 +0800

    JT-130: JT-132: book service finish book service category search

--- book-service/src/main/java/app/book/CategoryModule.java
+++ book-service/src/main/java/app/book/CategoryModule.java
@@ -1,9 +1,12 @@
 package app.book;
 
 import app.book.api.BOCategoryWebService;
+import app.book.api.CategoryWebService;
 import app.book.category.service.BOCategoryService;
+import app.book.category.service.CategoryService;
 import app.book.category.web.BOCategoryWebServiceImpl;
 import app.book.category.domain.Category;
+import app.book.category.web.CategoryWebServiceImpl;
 import core.framework.module.Module;
 
 /**
@@ -14,6 +17,8 @@ public class CategoryModule extends Module {
     protected void initialize() {
         db().repository(Category.class);
         bind(BOCategoryService.class);
+        bind(CategoryService.class);
         api().service(BOCategoryWebService.class, bind(BOCategoryWebServiceImpl.class));
+        api().service(CategoryWebService.class, bind(CategoryWebServiceImpl.class));
     }
 }
---
+++ book-service/src/main/java/app/book/category/web/CategoryWebServiceImpl.java
@@ -0,0 +1,20 @@
+package app.book.category.web;
+
+import app.book.api.CategoryWebService;
+import app.book.api.category.SearchCategoryRequest;
+import app.book.api.category.SearchCategoryResponse;
+import app.book.category.service.CategoryService;
+import core.framework.inject.Inject;
+
+/**
+ * @author meow
+ */
+public class CategoryWebServiceImpl implements CategoryWebService {
+    @Inject
+    CategoryService service;
+
+    @Override
+    public SearchCategoryResponse search(SearchCategoryRequest request) {
+        return service.search(request);
+    }
+}
