35182b92597a30625ad853aa9e2b1f4cc52b0aa6
Meow <liuguozhu720@gmail.com>
2020-06-24 11:14:20 +0800

    JT-130: JT-134: borrowed record service & notification service borrowed record service mongodb migration

---
+++ borrow-record-mongo-migration/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import com.mongodb.client.model.Indexes;
+import core.framework.mongo.MongoMigration;
+import org.bson.Document;
+
+/**
+ * @author zoo
+ */
+public class Main {
+    public static void main(String[] args) {
+        MongoMigration adminMigration = new MongoMigration("sys.properties", "sys.mongo.adminURI");
+        adminMigration.migrate(mongo -> {
+            mongo.runCommand(new Document().append("setParameter", 1).append("notablescan", 1));
+        });
+
+        MongoMigration migration = new MongoMigration("sys.properties");
+        migration.migrate(mongo -> {
+            mongo.createIndex("borrow_records", Indexes.ascending("borrower_id"));
+            mongo.createIndex("borrow_records", Indexes.ascending("book_id"));
+        });
+    }
+}
