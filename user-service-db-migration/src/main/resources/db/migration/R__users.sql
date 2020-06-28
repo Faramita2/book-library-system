774816a62c7b31997bf552ed8bf8580f61642d18
Meow <liuguozhu720@gmail.com>
2020-06-28 16:11:13 +0800

    JT-130: JT-133: user service && admin service users table add `email` column

--- user-service-db-migration/src/main/resources/db/migration/R__users.sql
+++ user-service-db-migration/src/main/resources/db/migration/R__users.sql
@@ -2,11 +2,39 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
     `username` VARCHAR(50) NOT NULL,
     `password` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
     `salt` VARCHAR(50) NOT NULL,
     `status` VARCHAR(50) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `created_by` VARCHAR(50) NOT NULL,
     `updated_by` VARCHAR(50) NOT NULL,
-    UNIQUE INDEX `username_index`(`username` ASC)
+    UNIQUE INDEX `username_index`(`username` ASC),
+    INDEX `email_index`(`email` ASC)
 );
+
+-- add column `email` if not exists.
+set @columnExisting := (
+    select count(COLUMN_NAME) from information_schema.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'users' AND COLUMN_NAME = 'email'
+);
+
+set @sqlStmt := if(
+    @columnExisting = 0,
+	"ALTER TABLE `users` ADD COLUMN `email` VARCHAR(50) NOT NULL",
+    "SELECT 'INFO: Column exists.'"
+);
+
+PREPARE stmt FROM @sqlStmt;
+EXECUTE stmt;
+
+-- add index `email` if not exists.
+set @indexExist := (
+    select count(*) from information_schema.STATISTICS where table_name = 'users' and index_name = 'email_index' and table_schema = database()
+);
+set @sqlStmt := if(
+    @indexExist = 0,
+    "ALTER TABLE `users` ADD INDEX `email_index` (`email` ASC);",
+    "select 'INFO: Index exists.'"
+);
+PREPARE stmt FROM @sqlStmt;
+EXECUTE stmt;
